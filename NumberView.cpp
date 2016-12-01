919140b2828f8abc261a9b1c12197805e06d06bb
Mason X <masonx.01@gmail.com>
2016-12-01 01:31:49 +0000

    Proper syntax; increase font size a bit

--- NumberView.cpp
+++ NumberView.cpp
@@ -40,27 +40,33 @@ NumberView::Draw(BRect r)
 	BString c;
 	c << fNumber;
 	float width = StringWidth(c.String());
-	rgb_color color_foreground = {255,255,255}, color_background;
+	rgb_color color_foreground = {255,255,255,0}, color_background;
+	const static rgb_color cell2_bg = {238,228,218,0}, cell4_bg = {237,224,200,0},
+		cell8_bg = {242,177,121,0}, cell16_bg = {245,149,99,0}, cell32_bg = {246,124,95,0},
+		cell64_bg = {246,94,59,0}, cell128_bg = {237,207,114,0}, cell256_bg = {237,204,97,0},
+		cell512_bg = {237,200,80,0}, cell1024_bg = {237,197,63,0}, cell2048P_bg = {237,194,46,0},
+		cell_dark_fg = {119,110,101};
 	
 	switch (fNumber) {
-	case 2: color_background = {238,228,218}; color_foreground = {119,110,101}; break;
-	case 4: color_background = {237,224,200}; color_foreground = {119,110,101}; break;
-	case 8: color_background = {242,177,121}; break;
-	case 16: color_background = {245,149,99}; break;
-	case 32: color_background = {246,124,95}; break;
-	case 64: color_background = {246,94,59}; break;
-	case 128: color_background = {237,207,114}; break;
-	case 256: color_background = {237,204,97}; break;
-	case 512: color_background = {237,200,80}; break;
-	case 1024: color_background = {237,197,63}; break;
-	default: color_background = {237,194,46}; break;
+	case 2: color_background = cell2_bg; color_foreground = cell_dark_fg; break;
+	case 4: color_background = cell4_bg; color_foreground = cell_dark_fg; break;
+	case 8: color_background = cell8_bg; break;
+	case 16: color_background = cell16_bg; break;
+	case 32: color_background = cell32_bg; break;
+	case 64: color_background = cell64_bg; break;
+	case 128: color_background = cell128_bg; break;
+	case 256: color_background = cell256_bg; break;
+	case 512: color_background = cell512_bg; break;
+	case 1024: color_background = cell1024_bg; break;
+	default: color_background = cell2048P_bg; break;
 	}
 	
 	SetHighColor(color_background);
 	FillRect(bounds);
 	SetHighColor(color_foreground);
+	SetFontSize(30);
 	DrawString(c.String(),
-		BPoint(bounds.left + 50 - width / 2, bounds.bottom - 50 + 10));
+		BPoint(bounds.left + 46 - width / 2, bounds.bottom - 46 + 10));
 }
 
 NumberView::~NumberView()
